23cfe9aa48c1d94f799ecc9cc51e80f18cfd78f8
Dani Hoskins <hoskins.dannala@gmail.com>
2024-03-02 19:13:38 -0600

    Modules added

--- quiz.js
+++ quiz.js
@@ -1,2 +1,8 @@
+const prompt = require("prompt-sync");
+//Ask user for their name 
 let quizTaker = prompt("Introduce yourself:");
+    
+    let response = quizTaker.ToLowerCase(`Hello + ${quizTaker}`);
 console.log("Hello " + quizTaker);
+
+
