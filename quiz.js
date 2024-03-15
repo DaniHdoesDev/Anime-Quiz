6d8283ee559e2d291a235bf6b5f779bdac773ef6
Dani Hoskins <hoskins.dannala@gmail.com>
2024-03-14 21:22:06 -0500

    New background added

--- quiz.js
+++ quiz.js
@@ -1,8 +1,13 @@
-const prompt = require("prompt-sync");
-//Ask user for their name 
-let quizTaker = prompt("Introduce yourself:");
-    
-    let response = quizTaker.ToLowerCase(`Hello + ${quizTaker}`);
-console.log("Hello " + quizTaker);
+//const secretMsg = prompt("This is a top secret mission. If you choose to accept this mission, type your favorite color.")
+//let welcomeMsg = 
+
+let maxNum = Math.floor(Math.random()* 10) +1 ;
+console.log(maxNum);
+//function numMakeup(maxNum){
+   // return Math.floor(Math.random() * maxNum ) + 1;
+   // console.log(numMakeup())
+//}
 
 
+
+let resetButton;
